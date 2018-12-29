fe8c37ff5f6d4e0ad3868d59adedc67bf72d117a
HangDh <hangiedh@gmail.com>
2018-12-29 15:40:40 +0100

    Open AI - CartPole.

---
+++ ML_Sentdex/un_hierarchical.py
@@ -0,0 +1,35 @@
+import numpy as np
+from sklearn.cluster import MeanShift
+from sklearn.datasets.samples_generator import make_blobs
+import matplotlib.pyplot as plt
+
+centers = [[1,1], [5,5], [8,2]]
+
+X, y = make_blobs(n_samples = 300, centers = centers, cluster_std = 1)
+
+plt.scatter(X[:,0], X[:,1])
+plt.show()
+
+ms = MeanShift() # można uzywac parametru bandwidth - ktory okresli jak bardzo punkty moga nie przynalezec.
+ms.fit(X)
+
+labels = ms.labels_  # Nie pomylic sie z 'y' chociaz to tez 'labels'
+cluster_centers = ms.cluster_centers_
+
+print(cluster_centers)
+
+n_clusters_ = len(np.unique(labels))
+
+print("Number of estimated clusters", n_clusters_)
+
+colors = 10*['r.','g.','b.', 'c.', 'k.', 'w.', 'y.', 'm.'] # zrobi 10x tą listę, zeby nie brakło kolorów
+
+for i in range(len(X)):
+    plt.plot(X[i][0], X[i][1], colors[labels[i]], markersize=5)
+
+plt.scatter(cluster_centers[:,0], cluster_centers[:,1],
+            marker="x", s=150, linewidths=5, zorder=10)
+
+plt.show()
+
+
